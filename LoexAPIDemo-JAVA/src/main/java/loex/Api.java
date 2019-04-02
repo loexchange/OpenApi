dce80c9cb9973ebbc7d39032e81612da69856121
loexchange <49189525+loexchange@users.noreply.github.com>
2019-04-02 17:28:42 +0800

    Update Api.java

--- LoexAPIDemo-JAVA/src/main/java/loex/Api.java
+++ LoexAPIDemo-JAVA/src/main/java/loex/Api.java
@@ -47,9 +47,9 @@ public class Api {
     private static String bytesToHex(byte[] md5Array) {
         StringBuilder strBuilder = new StringBuilder();
         for (int i = 0; i < md5Array.length; i++) {
-            int temp = 0xff & md5Array[i];//TODO:此处为什么添加 0xff & ？
+            int temp = 0xff & md5Array[i];
             String hexString = Integer.toHexString(temp);
-            if (hexString.length() == 1) {//如果是十六进制的0f，默认只显示f，此时要补上0
+            if (hexString.length() == 1) {
                 strBuilder.append("0").append(hexString);
             } else {
                 strBuilder.append(hexString);
